7894705a30d54b663bd57fbb0938fb556fe3d19a
Marleneluelmo <marleneluelmo@gmail.com>
2021-12-09 10:27:06 +0100

    deleted paths villains neutrals heores

--- src/router/index.js
+++ src/router/index.js
@@ -11,33 +11,6 @@ const routes = [
     component: Home,
   },
   {
-    path: "/heroes",
-    name: "Heroes",
-    // route level code-splitting
-    // this generates a separate chunk (heroes.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "Heroes" */ "../views/Heroes.vue"),
-  },
-  {
-    path: "/villanos",
-    name: "Villains",
-    // route level code-splitting
-    // this generates a separate chunk (villanos.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "Villains" */ "../views/Villains.vue"),
-  },
-  {
-    path: "/neutros",
-    name: "Neutrals",
-    // route level code-splitting
-    // this generates a separate chunk (neutros.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "Neutrals" */ "../views/Neutrals.vue"),
-  },
-  {
     path: "/favoritos",
     name: "Favourites",
     // route level code-splitting
