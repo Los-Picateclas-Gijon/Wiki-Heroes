4a1e22d493f76d874884901527bd9c18da133309
caiodst <caiodasilvatx@gmail.com>
2021-12-09 15:14:30 +0100

    created fetch function for the API, title and img

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,15 +9,9 @@ export default {
   components: {
     Gallery,
   },
-  data: () => ({
-    heroesList: [],
-  }),
-  methods: {},
-  created: async () => {
-    const url = "https://akabab.github.io/superhero-api/api/all.json";
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data[0].name);
+  data: () => ({}),
+  props: {
+    card: Object,
   },
 };
 </script>
